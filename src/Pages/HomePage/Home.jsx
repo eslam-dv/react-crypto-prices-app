175104f6e4c10dc2896a251d1c45d477fec1599f
Eslam Osama <eslamebrahim1301@gmail.com>
2024-03-16 11:47:29 +0200

    add loading and no data found

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -6,24 +6,25 @@ import "./home.css";
 export const Home = () => {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getAllCoins = async () => {
-      try {
-        const response = await fetch(
-          "/api/markets?vs_currency=usd&per_page=10"
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const data = await response.json();
-        setCoins(data);
-      } catch (err) {
-        console.error("Error fetching data:", err);
+  const getAllCoins = async () => {
+    try {
+      const response = await fetch("/api/markets?vs_currency=usd&per_page=10");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
       }
-    };
+      const data = await response.json();
+      setCoins(data);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     getAllCoins();
   }, []);
 
@@ -51,30 +52,35 @@ export const Home = () => {
           <p className="hide-mobile">Volume</p>
           <p className="hide-mobile">Market cap</p>
         </div>
-
-        {filteredCoins.map((coin, index) => (
-          <div
-            key={index}
-            className="coin-row"
-            onClick={() => navigate(`/coin/${coin.id}`)}
-          >
-            <p>{coin.market_cap_rank}</p>
-            <div className="img-symbol">
-              <img src={coin.image} alt={coin.id} />
-              <p>{coin.symbol.toUpperCase()}</p>
-            </div>
-            <p>${coin.current_price}</p>
-            <p
-              className={`${
-                coin.price_change_percentage_24h < 0 ? "red" : "green"
-              }`}
+        {loading ? (
+          <p className="loading">Loading...</p>
+        ) : coins.length > 0 ? (
+          filteredCoins.map((coin, index) => (
+            <div
+              key={index}
+              className="coin-row"
+              onClick={() => navigate(`/coin/${coin.id}`)}
             >
-              {coin.price_change_percentage_24h.toFixed(2)}%
-            </p>
-            <p className="hide-mobile">{coin.total_volume}</p>
-            <p className="hide-mobile">{coin.market_cap}</p>
-          </div>
-        ))}
+              <p>{coin.market_cap_rank}</p>
+              <div className="img-symbol">
+                <img src={coin.image} alt={coin.id} />
+                <p>{coin.symbol.toUpperCase()}</p>
+              </div>
+              <p>${coin.current_price}</p>
+              <p
+                className={`${
+                  coin.price_change_percentage_24h < 0 ? "red" : "green"
+                }`}
+              >
+                {coin.price_change_percentage_24h.toFixed(2)}%
+              </p>
+              <p className="hide-mobile">{coin.total_volume}</p>
+              <p className="hide-mobile">{coin.market_cap}</p>
+            </div>
+          ))
+        ) : (
+          <p className="no-data">No data found.</p>
+        )}
       </section>
     </main>
   );
